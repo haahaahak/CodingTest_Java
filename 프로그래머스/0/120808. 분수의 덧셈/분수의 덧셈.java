c8c825e8b00fa9616ae5a62ed3f0f507d9cc58fe
HAK <135566677+haahaahak@users.noreply.github.com>
2024-12-09 23:45:33 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -1,19 +1,21 @@
 class Solution {
+    
+    public int gcd(int num1, int num2) {
+        if (num2 == 0)
+            return num1;
+        return gcd(num2, num1 % num2);
+    }
+    
     public int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = new int[2];
         
         int numer = (numer1 * denom2) + (numer2 * denom1);
         int denom = denom1 * denom2;
         
-        for (int i = numer-1; i > 1; i--) {
-            if (numer % i == 0 && denom % i == 0) {
-                numer /= i;
-                denom /= i;
-            }
-        }
-        
-        answer[0] = numer;
-        answer[1] = denom;
+        int gcdValue = gcd(numer, denom);
+
+        answer[0] = numer/gcdValue;
+        answer[1] = denom/gcdValue;
         
         return answer;
     }
