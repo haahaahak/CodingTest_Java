74a2ceb35a75a1b82a60048837516c139c332e23
HAK <135566677+haahaahak@users.noreply.github.com>
2024-12-14 19:38:49 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n) {        
+        int size = n % 2 == 0 ? n / 2 : n / 2 + 1;
+        
+        int[] answer = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            answer[i] = (i * 2) + 1;
+        }
+               
+        return answer;
+    }
+}
