4fcec23a655234b1946796079e9c6e3357d38f65
HAK <135566677+haahaahak@users.noreply.github.com>
2024-12-17 11:09:30 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] solution(int[] array) {
         int[] answer = new int[2];
-        answer[0] = 0;
-        int index = 0;
         
-        for (int i: array) {
-            if (answer[0] < i) {
-                answer[0] = i;
-                answer[1] = index;
+        for (int i = 0; i < array.length; i++) {
+            if(answer[0] < array[i]) {
+                answer[0] = array[i];
+                answer[1] = i;
             }
-            index++;
-            
         }
         
         return answer;
