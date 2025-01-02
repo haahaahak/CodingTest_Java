c52f9e10cbd97512af28cf8c4fafe3263651dba0
HAK <135566677+haahaahak@users.noreply.github.com>
2025-01-02 12:32:50 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,7 +1,15 @@
-import java.util.Arrays;
-
 class Solution {
     public double solution(int[] arr) {
-        return Arrays.stream(arr).average().orElse(0);
+        double answer = 0;
+        
+        int sum = 0;
+        
+        for (int i: arr) {
+            sum += i;
+        }
+        
+        answer = (double) sum / arr.length;
+        
+        return answer;
     }
 }
