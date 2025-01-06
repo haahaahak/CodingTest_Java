b78e1e3128edf219c4e4118fe74a4a1271d0d44b
HAK <135566677+haahaahak@users.noreply.github.com>
2025-01-06 10:31:41 +0900

    [level 0] Title: 수 조작하기 2, Time: 14.22 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,21 +1,16 @@
 class Solution {
     public String solution(int[] numLog) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
         
         for (int i = 1; i < numLog.length; i++) {
-            answer += cal(numLog[i], numLog[i - 1]);
+            int diff = numLog[i] - numLog[i - 1];
+            
+            if (diff == 1) answer.append("w");
+            else if (diff == -1) answer.append("s");
+            else if (diff == 10) answer.append("d");
+            else if (diff == -10) answer.append("a");
         }
         
-        return answer;
-    }
-    
-    private String cal(int num, int prev) {
-        switch (num - prev) {
-            case 1: return "w";
-            case -1: return "s";
-            case 10: return "d";
-            case -10: return "a";
-            default: return "";
-        }
+        return answer.toString();
     }
 }
