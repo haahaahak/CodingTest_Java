df986ec00a6ce70b6a28138452ef2a62d75d6ea2
HAK <135566677+haahaahak@users.noreply.github.com>
2025-01-07 20:58:40 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] common) {       
+        int diff = common[1] - common[0];
+        
+        if (diff == common[2] - common[1]) {
+            return common[common.length - 1] + diff; 
+        }
+        
+        int ratio = common[1] / common[0];
+        
+        return common[common.length - 1] * ratio;
+    }
+}
