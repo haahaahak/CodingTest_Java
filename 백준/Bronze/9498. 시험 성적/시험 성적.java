e6fc27d63fe7a14baca9a90df7e3a652d9084bd5
HAK <135566677+haahaahak@users.noreply.github.com>
2025-01-20 17:32:39 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int point = Integer.parseInt(br.readLine());
+        String grade;
+        
+        if (point >= 90) {
+            grade = "A";
+        } else if (point >= 80) {
+            grade = "B";
+        } else if (point >= 70) {
+            grade = "C";
+        } else if (point >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+        
+        System.out.println(grade);
+    }
+}
