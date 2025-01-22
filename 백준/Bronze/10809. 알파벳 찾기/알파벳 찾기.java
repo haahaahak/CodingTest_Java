92861516e824d572a8a6bd036d1bac2db8dc4011
HAK <135566677+haahaahak@users.noreply.github.com>
2025-01-22 12:42:47 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 112 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        String line = br.readLine();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(line.indexOf(ch)).append(" ");
+        }
+        System.out.print(sb.toString());
+    }
+}
