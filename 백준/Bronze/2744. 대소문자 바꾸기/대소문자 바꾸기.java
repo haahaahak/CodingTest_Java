5ae22633af9156668acf82cbeac70cfe24aa8264
HAK <135566677+haahaahak@users.noreply.github.com>
2025-02-05 14:30:12 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 100 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+        String line = br.readLine();
+        for (char ch : line.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
+                sb.append(Character.toLowerCase(ch));
+            } else {
+                sb.append(Character.toUpperCase(ch));
+            }
+        }
+
+        System.out.print(sb.toString());
+	}
+}
