ae13548ea509ba0683510f7808e64e7f04f3ac60
HAK <135566677+haahaahak@users.noreply.github.com>
2025-03-03 15:14:09 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -4,22 +4,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
+        int q = 25;
+        int d = 10;
+        int n = 5;
+        // int p = 1;
+        
         int T = Integer.parseInt(br.readLine());
         
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < T; i++) {
             int changes = Integer.parseInt(br.readLine());
             
-            int q = changes / 25;
-            changes %= 25;
-            
-            int d = changes / 10;
-            changes %= 10;
-            
-            int n = changes / 5;
-            changes %= 5;
-            
-            System.out.println(q + " " + d + " " + n + " " + changes);
+            sb.append(changes / q).append(" ");
+            changes %= q;
+            sb.append(changes / d).append(" ");
+            changes %= d;
+            sb.append(changes / n).append(" ");
+            changes %= n;
+            sb.append(changes).append("\n");
         }
+        
+        System.out.print(sb);
     }
 }
