ad3fa60ddf148cad7ed5e3175a35dbedc779e8ac
HAK <135566677+haahaahak@users.noreply.github.com>
2025-03-06 19:56:02 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                sb.append(i).append("\n");
+                N /= i;
+            }
+        }
+        
+        if (N > 1) {
+            sb.append(N).append("\n");
+        }
+        
+        System.out.print(sb);
+    }
+}
