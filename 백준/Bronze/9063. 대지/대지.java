3e883d55f3077d916afec0f99dd3da5ca54d5b05
HAK <135566677+haahaahak@users.noreply.github.com>
2025-03-07 12:28:55 +0900

    [Bronze III] Title: 대지, Time: 252 ms, Memory: 27492 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        // N이 1이면 0출력 후 종료
+        if (N == 1) {
+            System.out.print(0);
+            return;
+        }
+        
+        int minx = Integer.MAX_VALUE;
+        int miny = Integer.MAX_VALUE;
+        int maxx = Integer.MIN_VALUE;
+        int maxy = Integer.MIN_VALUE;
+        
+        StringTokenizer st;
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            minx = Math.min(minx, x);
+            miny = Math.min(miny, y);
+            maxx = Math.max(maxx, x);
+            maxy = Math.max(maxy, y);
+        }
+        
+        int area = (maxx - minx) * (maxy - miny);
+        System.out.print(area);
+    }
+}
